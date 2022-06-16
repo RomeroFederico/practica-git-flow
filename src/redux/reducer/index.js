ae2452a4acd253d30e7df0ff4dbff50ac93d8440
Federico <romerof14@gmail.com>
2022-06-16 14:55:26 -0300

    DEVELOP: agregado redux.

---
+++ src/redux/reducer/index.js
@@ -0,0 +1,12 @@
+const initialState = {
+
+};
+
+const rootReducer = function(state = initialState, { type, payload }) {
+  switch(type) {
+    default:
+      return state;
+  }
+}
+
+export default rootReducer;
